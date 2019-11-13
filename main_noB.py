377f717da99955563cf609c8c067abd3359b374b
chervias <carlosherviascaimapo@wc-dhcp66d209.employee-secure.wireless.fsu.edu>
2019-11-13 15:53:22 -0500

    Nov 13th

--- main_noB.py
+++ main_noB.py
@@ -13,6 +13,7 @@ output_tqumap	= 'test_ns512_Nf10k_maa35_sr0p25_sl4p6.fits'
 nside			= 512
 Npix_box		= 256
 theta_LH_RMS	= 35.0 # in degrees
+size_scale		= 0.1
 size_ratio		= 0.25
 Nfil			= 10000
 size_box		= 1500.0 # physical size box
@@ -21,7 +22,7 @@ sky				= Sky(nside)
 # Create the magnetic field object
 magfield		= MagField_noB(size_box,'magfield_Bcube.npy')
 # Create the filament population object
-population		= FilPop(Nfil,theta_LH_RMS,size_ratio,magfield)
+population		= FilPop(Nfil,theta_LH_RMS,size_ratio,size_scale,magfield)
 
 data	= [(n,sky,population,magfield) for n in range(Nfil)]
 p 		= mp.Pool(processes=32)
